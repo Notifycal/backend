fc8e00698b363efd7f46b8c8d30c45e9736375df
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-04-26 23:59:48 +0200

    ci: clear dist/ before unzipping

--- ci/pre-plan-apply.sh
+++ ci/pre-plan-apply.sh
@@ -5,6 +5,7 @@ if [[ "$DEBUG" == true ]]; then
 fi
 
 _GH_ORG="Notifycal"
+OUT_DIR=dist
 
 # Default TF_TOOL is terragrunt
 TF_TOOL="${TF_TOOL:-terragrunt}"
@@ -31,7 +32,8 @@ echo "Retrieving release from Github..."
 TMP_DIR=$(mktemp -d "/tmp/${STACK_NAME}.XXXXX")
 gh release download "${STACK_VERSION}" --repo "${_GH_ORG}/${STACK_NAME}" --dir "${TMP_DIR}"
 
-unzip "${TMP_DIR}/build.zip" -d dist/
+rm -rf "${OUT_DIR}"
+unzip "${TMP_DIR}/build.zip" -d "${OUT_DIR}/"
 echo -e "Download finished!\n"
 
 echo "Removing temp folder..."
