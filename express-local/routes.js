345614e950d60600b8ded69a151cf913f8e6b839
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-05-07 01:07:57 +0200

    fix: use CommonJS modules instead of ESM as support is crap

--- express-local/routes.js
+++ express-local/routes.js
@@ -1,7 +1,7 @@
 // Lambdas are imported here (from dist folder, so all dependencies are bundled).
 // It'd be nice if it could be generated off the OpenAPI spec but... this is it for now
-import { handler as getCalendarEvents } from '../dist/api/get-calendar-events/index.mjs';
-import { handler as postWatchEvents } from '../dist/api/post-watch-events/index.mjs';
+import { handler as getCalendarEvents } from '../dist/api/get-calendar-events/index.cjs';
+import { handler as postWatchEvents } from '../dist/api/post-watch-events/index.cjs';
 
 const routes = {
   watchEvent: {
--- shim.ts
+++
@@ -1,5 +0,0 @@
-import { createRequire } from 'node:module';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import buffer from 'node:buffer';
-
-globalThis.require = createRequire(import.meta.url);
