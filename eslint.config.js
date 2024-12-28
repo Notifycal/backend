513701d0ea43eba36e746f48d0d6c2230710b3b6
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-12-28 19:21:15 +0100

    chore: use a tighter eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,155 @@
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import jestPlugin from 'eslint-plugin-jest';
-import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-export default [
-  {
-    files: ['**/*.ts', '**/*.js'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2020,
-      sourceType: 'module'
+import { fixupPluginRules } from '@eslint/compat';
+
+import eslintJS from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import typescriptEslint from 'typescript-eslint';
+
+// import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import jestPlugin from 'eslint-plugin-jest';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const patchedImportPlugin = fixupPluginRules(eslintPluginImport);
+
+const lintableFiles = ['src/**/*.ts', 'src/**/*.js'];
+
+const baseESLintConfig = {
+  name: 'eslint',
+  extends: [eslintJS.configs.recommended],
+  files: lintableFiles,
+  rules: {
+    'no-await-in-loop': 'error',
+    'no-constant-binary-expression': 'error',
+    'no-duplicate-imports': 'error',
+    'no-new-native-nonconstructor': 'error',
+    'no-promise-executor-return': 'error',
+    'no-self-compare': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-unreachable-loop': 'error',
+    'no-unused-private-class-members': 'error',
+    'no-use-before-define': 'error',
+    'require-atomic-updates': 'error',
+    camelcase: 'error'
+  }
+};
+
+const typescriptConfig = {
+  name: 'typescript',
+  extends: [...typescriptEslint.configs.recommendedTypeChecked],
+  files: lintableFiles,
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaFeatures: { modules: true },
+      ecmaVersion: '2020',
+      project: './tsconfig.json'
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      jest: jestPlugin,
-      prettier: prettierPlugin
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      ...tsPlugin.configs.stylistic.rules,
-      ...jestPlugin.configs.recommended.rules,
-      ...prettierPlugin.configs.recommended.rules,
-      // Mind, there is a jest plugin to fail any test not containing assertions. Jest Linting is not clever enough to beware the assertion is embeded in some function.
-      'jest/expect-expect': [
-        'error',
-        {
-          assertFunctionNames: ['expect', 'assert']
-        }
-      ]
-    },
-    settings: {
-      jest: {
-        version: 'detect'
-      }
+    globals: {
+      ...globals.builtin,
+      ...globals.browser,
+      ...globals.es2025
     }
   },
-  // GOTCHA: this block needs to be separated so it takes effect
-  {
-    ignores: ['dist/**/*', 'eslint.config.js', 'tf/**/*']
+  linterOptions: {
+    reportUnusedDisableDirectives: 'error'
+  },
+  plugins: {
+    import: patchedImportPlugin
+  },
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-confusing-void-expression': 'error',
+    '@typescript-eslint/no-import-type-side-effects': 'error',
+    '@typescript-eslint/no-require-imports': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-useless-empty-export': 'error',
+    '@typescript-eslint/prefer-enum-initializers': 'error',
+    '@typescript-eslint/prefer-readonly': 'error',
+    'no-return-await': 'off',
+    '@typescript-eslint/return-await': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false
+        }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json'
+      }
+    }
   }
-];
+};
+
+const unicornConfig = {
+  name: 'unicorn',
+  plugins: {
+    unicorn: eslintPluginUnicorn
+  },
+  files: lintableFiles,
+  rules: {
+    'unicorn/custom-error-definition': 'error',
+    'unicorn/empty-brace-spaces': 'error',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-console-spaces': 'error',
+    'unicorn/no-null': 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/prevent-abbreviations': 'off'
+  }
+};
+
+const jestConfig = {
+  name: 'jest',
+  plugins: {
+    jest: jestPlugin,
+  },
+  files: ["**/*.test.ts", "**/*.spec.ts"],
+  languageOptions: {
+    globals: {
+      ...jestPlugin.globals
+    }
+  },
+  rules: {
+    ...jestPlugin.configs.recommended.rules,
+    // Mind, there is a jest plugin to fail any test not containing assertions. Jest Linting is not clever enough to beware the assertion is embeded in some function.
+    'jest/expect-expect': [
+      'error',
+      {
+        assertFunctionNames: ['expect', 'assert']
+      }
+    ]
+  },
+  settings: {
+    jest: {
+      version: 'detect'
+    }
+  }
+};
+
+const eslintConfig = typescriptEslint.config(
+  baseESLintConfig,
+  typescriptConfig,
+  eslintConfigPrettier,
+  unicornConfig,
+  jestConfig
+);
+
+export default eslintConfig;
+
