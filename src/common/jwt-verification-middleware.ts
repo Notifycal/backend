aaffc686ad0abcbea91eda27126ba9cd734e2ba9
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-02 14:04:14 +0100

    fix: patch middleware impl to handle errors. middy works in a different way then I thought

--- src/common/jwt-verification-middleware.ts
+++ src/common/jwt-verification-middleware.ts
@@ -1,13 +1,11 @@
 import { AuthedEventWithConfig } from '../model/ApiGatewayEvents';
 import middy, { MiddlewareObj, Request } from '@middy/core';
-import httpErrorHandler from '@middy/http-error-handler';
-import createHttpError from 'http-errors';
 import { APIGatewayProxyStructuredResultV2, Context } from 'aws-lambda';
 import { decodeAndVerifyJwtSignature } from '@services/jwt';
 import { JwtClaimCheckerFn } from '@own-types/model';
 import { AuthedEndpointConfig } from '@model/Config';
-import { logger } from '@common/powertools';
 import { AccessToken, accessTokenSchema } from '@model/Jwt';
+import { errorHandler } from '@services/common/api-response-handlers';
 
 export function jwtVerificationMiddleware<TConfig extends AuthedEndpointConfig>(
   claimChecker: JwtClaimCheckerFn
@@ -16,10 +14,8 @@ export function jwtVerificationMiddleware<TConfig extends AuthedEndpointConfig>(
     AuthedEventWithConfig<TConfig>,
     APIGatewayProxyStructuredResultV2
   > = (req) => jwtVerification(req, claimChecker);
-  const onError = httpErrorHandler({ logger: (error) => logger.warn(error) }).onError;
   return {
-    before,
-    onError
+    before
   };
 }
 
@@ -31,11 +27,11 @@ function jwtVerification<TConfig extends AuthedEndpointConfig>(
     Context
   >,
   claimChecker: JwtClaimCheckerFn
-): void {
+): Promise<APIGatewayProxyStructuredResultV2 | void> {
   const headers = request.event.headers ?? {};
   const authorization = headers['Authorization'] || headers['authorization'];
   if (authorization) {
-    decodeAndVerifyJwtSignature(
+    return decodeAndVerifyJwtSignature(
       authorization.replace('Bearer ', ''),
       accessTokenSchema,
       request.event.requestContext.config.decodeAccessJwtConfig
@@ -44,30 +40,17 @@ function jwtVerification<TConfig extends AuthedEndpointConfig>(
         if (claimChecker(jwt)) {
           request.event.requestContext.authorizer = jwt;
         } else {
-          throw createHttpError(401, JSON.stringify({ message: 'Unauthorised' }), {
-            type: `Missing permissions to hit the API. Provided info: header = '${JSON.stringify(jwt.header)}' payload = '${JSON.stringify(jwt.payload)}'`,
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          });
+          return errorHandler(401)(
+            `Missing permissions to hit the API. Provided info: header = '${JSON.stringify(jwt.header)}' payload = '${JSON.stringify(jwt.payload)}'`
+          );
         }
       },
       (err) => {
-        throw createHttpError(401, JSON.stringify({ message: 'Unauthorised' }), {
-          type: `Invalid Signature. Error: ${JSON.stringify(err)}`,
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
+        return errorHandler(401)(`Invalid Signature. Error: ${JSON.stringify(err)}`);
       }
     );
   } else {
-    throw createHttpError(401, JSON.stringify({ message: 'Unauthorised' }), {
-      type: 'Missing Authorization',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    return Promise.resolve(errorHandler(401)('Missing Authorization'));
   }
 }
 
--- src/common/parser-http-middleware.ts
+++ src/common/parser-http-middleware.ts
@@ -1,11 +1,9 @@
 import { parser } from '@aws-lambda-powertools/parser/middleware';
 import middy, { MiddlewareObj } from '@middy/core';
-import httpErrorHandler from '@middy/http-error-handler';
 import { Request } from '@middy/core';
+import { errorHandler } from '@services/common/api-response-handlers';
 import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Context } from 'aws-lambda';
-import createHttpError from 'http-errors';
 import { ZodSchema } from 'zod';
-import { logger } from './powertools';
 
 export function httpRequestEventParserMiddleware(
   schema: ZodSchema
@@ -13,28 +11,23 @@ export function httpRequestEventParserMiddleware(
   const before: middy.MiddlewareFn<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2> = (
     req
   ) => httpRequestEventParser(req, schema);
-  const onError = httpErrorHandler({ logger: (error) => logger.warn(error) }).onError;
   return {
-    before,
-    onError
+    before
   };
 }
 
 function httpRequestEventParser(
   request: Request<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Error, Context>,
   schema: ZodSchema
-): void {
+): APIGatewayProxyStructuredResultV2 | void {
   const parserFn = parser({ schema }).before;
   if (parserFn) {
     try {
       parserFn(request);
     } catch (error) {
-      throw createHttpError(400, JSON.stringify({ message: 'Bad Request' }), {
-        type: `Request payload does not satisfy the schema. Error: ${error}. Schema: ${JSON.stringify(schema)}`,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+      return errorHandler(400)(
+        `Request payload does not satisfy the schema. Error: ${error}. Schema: ${JSON.stringify(schema)}`
+      );
     }
   }
 }
