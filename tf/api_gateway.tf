cdb10ca6db562354a568de3a693b476654d615d7
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-01-04 00:32:53 +0100

    reference stage rather than stage-less deployment

--- tf/api_gateway.tf
+++ tf/api_gateway.tf
@@ -47,7 +47,7 @@ resource "aws_api_gateway_stage" "stage" {
 
 resource "aws_api_gateway_method_settings" "method_settings" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  stage_name  = aws_api_gateway_deployment.api_deployment.stage_name
+  stage_name  = aws_api_gateway_stage.stage.stage_name
 
   # this means it's enabled for all paths
   method_path = "*/*"
