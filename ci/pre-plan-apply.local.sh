594b54f47ef5635276b152ea8c556acdee9211da
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-08 16:06:05 +0100

    fixes

--- ci/pre-plan-apply.local.sh
+++ ci/pre-plan-apply.local.sh
@@ -3,11 +3,9 @@
 if [[ "$DEBUG" == true ]]; then
   set -ex
 fi
-
-_GH_ORG="Notifycal"
-
-STACK_NAME=$1
-STACK_VERSION=$2
+STACK_NAME="$1"
+STACK_VERSION="$2"
+REPO_PATH=$(realpath $(dirname $(dirname "$0")))
 
 # running path is the working dir as this script makes "changes" in the
 # actual TF "execution folder"
@@ -18,13 +16,15 @@ echo "Running $0..."
 echo "==================================="
 echo "STACK NAME: ${STACK_NAME}"
 echo "STACK_VERSION: ${STACK_VERSION}"    # Assumes STACK_NAME == repository name
-echo "PATH: $RUNNING_PATH"
+echo "PWD: $RUNNING_PATH"
+echo "REPO_PATH: $REPO_PATH"
+echo "OUT_DIR: $OUT_DIR"
 echo "==================================="
 echo
 
 echo "Creating adhoc build..."
-pushd "${RUNNING_PATH}" > /dev/null
-npm run build && npm run package && pushd dist && unzip build.zip && popd
+pushd "${REPO_PATH}" > /dev/null
+npm run build && npm run package && pushd dist && unzip -o build.zip -d "$OUT_DIR" && popd
 popd > /dev/null
 
 exit 0;
