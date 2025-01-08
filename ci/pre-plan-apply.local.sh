50f75e52158026445d0c15a497256ddd946b38f3
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-01-08 16:10:53 +0100

    local: add ability to create adhoc build when deploying locally (#195)
    Co-authored-by: Sergio Martin Sanchez <sergio.anger@gmail.com>

---
+++ ci/pre-plan-apply.local.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ "$DEBUG" == true ]]; then
+  set -ex
+fi
+STACK_NAME="$1"
+STACK_VERSION="$2"
+REPO_PATH=$(realpath $(dirname $(dirname "$0")))
+
+# running path is the working dir as this script makes "changes" in the
+# actual TF "execution folder"
+RUNNING_PATH="$(pwd)"
+
+echo
+echo "Running $0..."
+echo "==================================="
+echo "STACK NAME: ${STACK_NAME}"
+echo "STACK_VERSION: ${STACK_VERSION}"    # Assumes STACK_NAME == repository name
+echo "PWD: $RUNNING_PATH"
+echo "REPO_PATH: $REPO_PATH"
+echo "OUT_DIR: $OUT_DIR"
+echo "==================================="
+echo
+
+echo "Creating adhoc build..."
+pushd "${REPO_PATH}" > /dev/null
+npm run build && npm run package && pushd dist && unzip -o build.zip -d "$OUT_DIR" && popd
+popd > /dev/null
+
+exit 0;
