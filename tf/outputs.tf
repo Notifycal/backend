c0c924c170a6f5548bdee84d681537cc96757b48
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-01-14 23:57:16 +0100

    fix: local development on localstack (#217)
    For completeness, since APIGW custom domain is created conditionally,
    set _service_registration_url accordingly if custom domain is not
    created and therefore standard APIGW endpoint is exposed
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -4,9 +4,5 @@ output "rendered_openapi_spec" {
 }
 
 locals {
-  _service_registration_url = format(
-    "https://%s.%s",
-    var.domain_prefix,
-    var.base_domain
-  )
+  _service_registration_url = var.api_gateway_custom_domain_enabled ? module.apigateway_custom_domain[0].invoke_url : aws_api_gateway_stage.stage.invoke_url
 }
