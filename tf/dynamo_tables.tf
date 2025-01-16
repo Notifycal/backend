0425c1b443ae1c336060aa92c1401bbe3338df6f
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-16 10:53:36 +0100

    revert: remove sort_key from users dynamo table as there is no clear usage short-term

--- tf/dynamo_tables.tf
+++ tf/dynamo_tables.tf
@@ -3,15 +3,10 @@ resource "aws_dynamodb_table" "users" {
   # TODO: Maybe change it to PROVISIONED in the future when the workloads are clear(er)
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "UserId"
-  range_key    = "IdpUserId"
 
   # Only need to define attributes that are part of the key (hash/range) or Secondary indices
   attribute {
     name = "UserId"
-    type = "S"
-  }
-  attribute {
-    name = "IdpUserId"
     type = "S"
   }
 
