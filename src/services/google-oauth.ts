430c65bc71dde9fcfd0d7ea872885832c61e98e0
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-01-19 23:56:18 +0100

    fix: google email verified check. Test coverage (#228)

--- src/services/google-oauth.ts
+++ src/services/google-oauth.ts
@@ -15,32 +15,35 @@ export function verifyGoogleIdentity(
 ): Promise<Identity> {
   const client = new OAuth2Client(config.clientId, config.clientSecret, config.redirectUri);
   return client.getToken(userGoogleCode).then((tokenResponse) => {
-    if (tokenResponse.tokens.id_token) {
-      return client
-        .verifyIdToken({
-          idToken: tokenResponse.tokens.id_token,
-          audience: config.clientId
-        })
-        .then((ticket) => {
-          const id = ticket.getUserId();
-          const email = ticket.getPayload()?.['email'];
-          if (id && email) {
-            const idpName: IdpName = 'google.com';
-            return {
-              userId: idGenerator(id, idpName),
-              email: email as Email,
-              idp: idpName,
-              idpId: id as IdpId
-            };
-          } else {
-            const msg = `Id and/or Email could not be extracted out of Google token id. Extracted id: '${id}' and email: '${email}'`;
-            throw new Error(msg);
-          }
-        });
-    } else {
+    if (!tokenResponse.tokens.id_token) {
       const msg =
         'Google token id was not present in token obtained from Google using user google,s code';
       throw new Error(msg);
     }
+    return client
+      .verifyIdToken({
+        idToken: tokenResponse.tokens.id_token,
+        audience: config.clientId
+      })
+      .then((ticket) => {
+        const id = ticket.getUserId();
+        const email = ticket.getPayload()?.['email'];
+        if (!ticket.getPayload()?.email_verified || !ticket.getPayload()?.email_verified) {
+          const msg = `Google user with id: '${id}' and email: '${email}' isn't verified at google. We cannot let them in.`;
+          throw new Error(msg);
+        }
+        if (id && email) {
+          const idpName: IdpName = 'google.com';
+          return {
+            userId: idGenerator(id, idpName),
+            email: email as Email,
+            idp: idpName,
+            idpId: id as IdpId
+          };
+        } else {
+          const msg = `Id and/or Email could not be extracted out of Google token id. Extracted id: '${id}' and email: '${email}'`;
+          throw new Error(msg);
+        }
+      });
   });
 }
