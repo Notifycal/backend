6ccb4eb1b73a10e8cf5b3209deab08d2e3e83d2d
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-01-21 01:54:53 +0100

    chore: more test coverage in id generator (#229)

--- src/services/id-generator.test.ts
+++ src/services/id-generator.test.ts
@@ -1,19 +1,19 @@
 import { describe, it, expect } from 'vitest';
 import { validate as validateUuid } from 'uuid';
-import { idGenerator } from './id-generator';
+import { hashString, idGenerator } from './id-generator';
 import type { IdpName } from '@model/Identity';
 
 describe('Id generator', () => {
   const idpName = 'google.com';
 
-  it('should generate a valid UUID v4 for a given input string', () => {
+  it('should generate a valid v5 UUID for a given input string and an Idp', () => {
     const input = 'test-input';
     const generatedId = idGenerator(input, idpName);
 
     expect(validateUuid(generatedId)).toBeTruthy();
   });
 
-  it('should generate the same UUID for the same input string', () => {
+  it('should generate the same UUID for the same input string and Idp', () => {
     const input = 'consistent-input';
     const id1 = idGenerator(input, idpName);
     const id2 = idGenerator(input, idpName);
@@ -21,11 +21,19 @@ describe('Id generator', () => {
     expect(id1).toStrictEqual(id2);
   });
 
-  it('should generate different UUIDs for different input strings', () => {
+  it('should generate different UUIDs for different input strings and same IdP', () => {
     const input1 = 'input-one';
     const input2 = 'input-two';
     const id1 = idGenerator(input1, idpName);
     const id2 = idGenerator(input2, idpName);
+
+    expect(id1).not.toStrictEqual(id2);
+  });
+
+  it('should generate different UUIDs for equal input strings and a different IdP', () => {
+    const input = 'consistent-input';
+    const id1 = idGenerator(input, idpName);
+    const id2 = idGenerator(input, 'ADifferentIdP' as IdpName);
 
     expect(id1).not.toStrictEqual(id2);
   });
@@ -56,6 +64,54 @@ describe('Id generator', () => {
   it('should not produce collisions for a large number of unique inputs', () => {
     const uniqueInputs = Array.from({ length: 100000 }, (_, i) => `input-${i}`);
     const generatedIds = uniqueInputs.map((input) => idGenerator(input, idpName));
+    const uniqueIdsSet = new Set(generatedIds);
+
+    expect(uniqueIdsSet.size).toStrictEqual(generatedIds.length);
+  });
+});
+
+describe('Hash string', () => {
+  it('should generate a valid UUID v4 for a given input string', () => {
+    const input = 'test-input';
+    const generatedId = hashString(input);
+
+    expect(validateUuid(generatedId)).toBeTruthy();
+  });
+
+  it('should generate the same UUID for the same input string', () => {
+    const input = 'consistent-input';
+    const id1 = hashString(input);
+    const id2 = hashString(input);
+
+    expect(id1).toStrictEqual(id2);
+  });
+
+  it('should generate different UUIDs for different input strings', () => {
+    const input1 = 'input-one';
+    const input2 = 'input-two';
+    const id1 = hashString(input1);
+    const id2 = hashString(input2);
+
+    expect(id1).not.toStrictEqual(id2);
+  });
+
+  it('should handle an empty string as input', () => {
+    const input = '';
+    const generatedId = hashString(input);
+
+    expect(validateUuid(generatedId)).toBeTruthy();
+  });
+
+  it('should handle large input strings', () => {
+    const input = 'a'.repeat(10_000);
+    const generatedId = hashString(input);
+
+    expect(validateUuid(generatedId)).toBeTruthy();
+  });
+
+  it('should not produce collisions for a large number of unique inputs', () => {
+    const uniqueInputs = Array.from({ length: 100000 }, (_, i) => `input-${i}`);
+    const generatedIds = uniqueInputs.map((input) => hashString(input));
     const uniqueIdsSet = new Set(generatedIds);
 
     expect(uniqueIdsSet.size).toStrictEqual(generatedIds.length);
