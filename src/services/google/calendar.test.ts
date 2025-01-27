e576e96fd48b570e52bd22082a81f43d25e0d94f
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-27 20:29:42 +0100

    fix: refine and unit test Google Calendar class

---
+++ src/services/google/calendar.test.ts
@@ -0,0 +1,159 @@
+/* eslint-disable camelcase */
+import type { Calendar } from '@model/Calendar';
+import type { CalendarId, CalendarName } from '@own-types/model';
+import { google, type calendar_v3 } from 'googleapis';
+import type { GaxiosResponse } from 'googleapis-common';
+import type { APIRequestContext } from 'googleapis/build/src/apis/abusiveexperiencereport';
+import { describe, expect, it, vi } from 'vitest';
+import { GoogleCalendar } from './calendar';
+
+describe('GoogleCalendar Service', () => {
+  const validGoogleCalendarListEntry: GaxiosResponse<calendar_v3.Schema$CalendarList> = {
+    data: {
+      items: [
+        { id: '1', summary: 'Calendar 1' },
+        { id: '2', summary: 'Calendar 2' }
+      ]
+    },
+    config: {},
+    status: 200,
+    statusText: '200OK',
+    headers: {},
+    request: {
+      responseURL: ''
+    }
+  };
+
+  it('should fetch the calendar list and return parsed data', () => {
+    const calendarListFn = () => Promise.resolve(validGoogleCalendarListEntry);
+
+    return testit(calendarListFn).then((result) => {
+      expect(result).toStrictEqual([
+        { id: '1' as CalendarId, name: 'Calendar 1' as CalendarName },
+        { id: '2' as CalendarId, name: 'Calendar 2' as CalendarName }
+      ]);
+    });
+  });
+
+  it('should throw an error if the calendar API fails', () => {
+    const error = new Error('Boom! Google API Failure');
+    const calendarListFn = () => Promise.reject(error);
+
+    const result = testit(calendarListFn);
+
+    return expect(result).rejects.toThrow(error.message);
+  });
+
+  it('should throw a custom error if parsing fails', () => {
+    const invalidGoogleCalendarListEntry: GaxiosResponse<calendar_v3.Schema$CalendarList> = {
+      data: {
+        items: [{ summary: 'Calendar 1' }, { id: '2' }]
+      },
+      config: {},
+      status: 200,
+      statusText: '200OK',
+      headers: {},
+      request: {
+        responseURL: ''
+      }
+    };
+    const calendarListFn = () => Promise.resolve(invalidGoogleCalendarListEntry);
+
+    const result = testit(calendarListFn);
+
+    return expect(result).rejects.toThrow(`Failed to parse Google Calendar List items with error. Error: [
+  {
+    "code": "invalid_type",
+    "expected": "string",
+    "received": "undefined",
+    "path": [
+      0,
+      "id"
+    ],
+    "message": "Required"
+  },
+  {
+    "code": "invalid_type",
+    "expected": "string",
+    "received": "undefined",
+    "path": [
+      1,
+      "name"
+    ],
+    "message": "Required"
+  }
+]`);
+  });
+
+  function testit(
+    calendarListFn: () => Promise<GaxiosResponse<calendar_v3.Schema$CalendarList>>
+  ): Promise<Array<Calendar>> {
+    vi.spyOn(google, 'calendar').mockReturnValue({
+      calendarList: {
+        list: vi.fn().mockImplementation(calendarListFn),
+        context: {} as APIRequestContext,
+        delete: vi.fn(),
+        get: vi.fn(),
+        insert: vi.fn(),
+        patch: vi.fn(),
+        update: vi.fn(),
+        watch: vi.fn()
+      },
+      context: {} as APIRequestContext,
+      acl: {
+        context: {} as APIRequestContext,
+        list: vi.fn(),
+        delete: vi.fn(),
+        get: vi.fn(),
+        insert: vi.fn(),
+        patch: vi.fn(),
+        update: vi.fn(),
+        watch: vi.fn()
+      },
+      calendars: {
+        context: {} as APIRequestContext,
+        delete: vi.fn(),
+        get: vi.fn(),
+        insert: vi.fn(),
+        patch: vi.fn(),
+        update: vi.fn(),
+        clear: vi.fn()
+      },
+      channels: {
+        context: {} as APIRequestContext,
+        stop: vi.fn()
+      },
+      colors: {
+        context: {} as APIRequestContext,
+        get: vi.fn()
+      },
+      events: {
+        context: {} as APIRequestContext,
+        list: vi.fn(),
+        delete: vi.fn(),
+        get: vi.fn(),
+        insert: vi.fn(),
+        patch: vi.fn(),
+        update: vi.fn(),
+        watch: vi.fn(),
+        import: vi.fn(),
+        instances: vi.fn(),
+        move: vi.fn(),
+        quickAdd: vi.fn()
+      },
+      freebusy: {
+        context: {} as APIRequestContext,
+        query: vi.fn()
+      },
+      settings: {
+        context: {} as APIRequestContext,
+        list: vi.fn(),
+        get: vi.fn(),
+        watch: vi.fn()
+      }
+    });
+
+    const config = { clientId: 'id', clientSecret: 'secret', redirectUri: 'uri' };
+    return GoogleCalendar.withRefreshToken(config, '').calendarList();
+  }
+});
--- src/services/google/calendar.ts
+++ src/services/google/calendar.ts
@@ -1,16 +1,14 @@
 /* eslint-disable camelcase */
-import { google, type calendar_v3 } from 'googleapis';
-import { OAuth2Client } from 'google-auth-library';
-import { throwError } from '@services/common/error-handling';
 import { calendarSchema, type Calendar } from '@model/Calendar';
+import type { GoogleOAuthConfig } from '@model/Config';
+import type { CalendarId, CalendarName } from '@own-types/model';
+import { throwError } from '@services/common/error-handling';
+import { OAuth2Client } from 'google-auth-library';
+import { google, type calendar_v3 } from 'googleapis';
 import { z } from 'zod';
 import { BaseGoogle } from './base-service';
-import type { GoogleOAuthConfig } from '@model/Config';
 
 export class GoogleCalendar extends BaseGoogle {
-  //   public constructor(config: GoogleOAuthConfig, refreshToken: string) {
-  //     super(config, refreshToken);
-  //   }
   public static withRefreshToken(config: GoogleOAuthConfig, refreshToken: string): GoogleCalendar {
     const client = new OAuth2Client(config.clientId, config.clientSecret, config.redirectUri);
 
@@ -19,24 +17,31 @@ export class GoogleCalendar extends BaseGoogle {
   }
 
   public calendarList(): Promise<Array<Calendar>> {
-    return this._calendarList().then((list) => this.toCalendar(list));
+    return this._calendarList().then((list) => this.toCalendarArray(list));
   }
 
-  private toCalendar(list: Array<calendar_v3.Schema$CalendarListEntry>): Promise<Array<Calendar>> {
+  private toCalendarEntry(item: calendar_v3.Schema$CalendarListEntry): Partial<Calendar> {
+    return {
+      id: item.id as CalendarId,
+      name: item.summary as CalendarName
+    };
+  }
+
+  private toCalendarArray(
+    list: Array<calendar_v3.Schema$CalendarListEntry>
+  ): Promise<Array<Calendar>> {
     // TODO: Test this in anger
-    return z.array(calendarSchema).promise().parse(list);
-    //   const validationResults = list.map((c) => {
-    //     const validationResult = calendarSchema.safeParse({
-    //       id: c.id,
-    //       name: c.summary
-    //     });
-    //     return validationResult;
-    //   });
-    //   if (validationResults.every((r) => r.success)) {
-    //     return validationResults.map((r) => r.data as z.infer<calendarSchema>);
-    //   } else {
-    //     return validationResults.filter()
-    //   }
+    const transformedList = list.map((i) => this.toCalendarEntry(i));
+    const parsingResult = z.array(calendarSchema).safeParse(transformedList);
+    if (parsingResult.success) {
+      return Promise.resolve(parsingResult.data);
+    } else {
+      return Promise.reject(
+        new Error(
+          `Failed to parse Google Calendar List items with error. Error: ${parsingResult.error.toString()}`
+        )
+      );
+    }
   }
 
   // CalendarEntryList Docs: https://developers.google.com/calendar/api/v3/reference/calendarList#resource
@@ -46,11 +51,10 @@ export class GoogleCalendar extends BaseGoogle {
     return calendar.calendarList
       .list()
       .then((response) => {
-        console.log(`${baseMsg}. ${JSON.stringify(response)}`);
         return response.data.items || [];
       })
       .catch((error) => {
-        throwError(`${baseMsg}. Error: ${error}`);
+        throwError(`${baseMsg}. ${error}`);
       });
   }
 }
