617683acdaf1288d07275f66ce9e002a36585d82
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-01 22:53:15 +0100

    query live users from GSI

--- tf/dynamo_users.tf
+++ tf/dynamo_users.tf
@@ -1,13 +1,36 @@
+locals {
+  users_table_name = "Users-${var.environment}"
+  live_users_index_name = "Live${local.users_table_name}"
+}
+
 resource "aws_dynamodb_table" "users" {
-  name = "Users-${var.environment}"
+  name = local.users_table_name
   # TODO: Maybe change it to PROVISIONED in the future when the workloads are clear(er)
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "UserId"
 
-  # Only need to define attributes that are part of the key (hash/range) or Secondary indices
   attribute {
     name = "UserId"
     type = "S"
+  }
+
+  attribute {
+    name = "UserStatus"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = local.live_users_index_name
+    hash_key = "UserStatus"
+    range_key = "UserId"
+    projection_type = "ALL"
+  }
+
+  # DROP THIS BEFORE MERGING: https://github.com/hashicorp/terraform-provider-aws/issues/41110
+  lifecycle {
+    ignore_changes = [
+      global_secondary_index
+    ]
   }
 
   deletion_protection_enabled = true
---
+++ tf/schedule_lambda_fetch_user_calendars.tf
@@ -0,0 +1,86 @@
+# Fetch user calendars or just fetch calendars? Isn't that implementation details?
+
+locals {
+  fetch_user_calendars_lambda_cron_schedule = "cron(0/5 * * * ? *)"
+  fetch_user_calendars_lambda_function_name      = "fetch-user-calendars"
+  fetch_user_calendars_lambda_function_full_name = "${local.fetch_user_calendars_lambda_function_name}-${var.environment}"
+}
+
+data "aws_iam_policy_document" "fetch_user_calendars_iam_policydoc" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:Query",
+    ]
+
+    resources = [
+      aws_dynamodb_table.users.arn,
+      "${aws_dynamodb_table.users.arn}/index/${local.live_users_index_name}"
+    ]
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "fetch_user_calendars_trigger_rule" {
+  name        = "${local.fetch_user_calendars_lambda_function_name}-schedule-${var.environment}"
+  description = "Schedule for fetch_user_calendars Lambda"
+
+  schedule_expression = local.fetch_user_calendars_lambda_cron_schedule
+}
+
+resource "aws_cloudwatch_event_target" "fetch_user_calendars_event_target" {
+  rule      = aws_cloudwatch_event_rule.fetch_user_calendars_trigger_rule.name
+  target_id = "GetUsers" # TODO: rename this
+
+  # Does this support alias ARN or just the base function?
+  arn       = module.fetch_user_calendars_lambda.lambda_function_arn
+}
+
+module "fetch_user_calendars_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 7.17"
+
+  function_name          = "fetch-user-calendars-${var.environment}"
+  publish                = local.lambdas_publish
+  create_package         = local.lambdas_create_package
+  local_existing_package = "${path.root}/../dist/lambdas/schedule/fetch-user-calendars.zip"
+
+  runtime     = var.lambdas_runtime
+  timeout     = local.api_lambdas_timeout
+  memory_size = 256
+  handler     = var.lambdas_handler_name
+
+  logging_log_format    = var.lambdas_logging_log_format
+  attach_tracing_policy = local.lambdas_attach_tracing_policy
+  tracing_mode          = var.lambdas_tracing_mode
+
+  maximum_retry_attempts = 0
+
+  tags = merge({
+    Schedule = local.fetch_user_calendars_lambda_cron_schedule
+  }, local.common_tags)
+
+  attach_policy_json = true
+  policy_json        = data.aws_iam_policy_document.fetch_user_calendars_iam_policydoc.json
+
+  environment_variables = merge({
+    LOCAL_USERS_INDEX_NAME = local.live_users_index_name
+    FETCH_CALENDARS_TOPIC_NAME = aws_sns_topic.calendars.id
+  }, local.protected_endpoint_env_vars, local.users_persistance_env_vars)
+}
+
+module "fetch_user_calendars_lambda_alias" {
+  source  = "terraform-aws-modules/lambda/aws//modules/alias"
+  version = "~> 7.17"
+
+  function_name    = module.fetch_user_calendars_lambda.lambda_function_name
+  function_version = module.fetch_user_calendars_lambda.lambda_function_version
+  name             = var.lambdas_live_alias_name
+
+  allowed_triggers = {
+    AllowEventBridgeInvoke = {
+      principal = "events.amazonaws.com"
+      source_arn = aws_cloudwatch_event_rule.fetch_user_calendars_trigger_rule.arn
+    }
+  }
+}
