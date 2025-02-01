73b9031097857d58ff263d514fba803efaf60d85
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-01 11:17:03 +0100

    chore: one file per dynamo table

--- tf/dynamo_refresh_tokens.tf
+++ tf/dynamo_refresh_tokens.tf
@@ -1,18 +1,3 @@
-resource "aws_dynamodb_table" "users" {
-  name = "Users-${var.environment}"
-  # TODO: Maybe change it to PROVISIONED in the future when the workloads are clear(er)
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "UserId"
-
-  # Only need to define attributes that are part of the key (hash/range) or Secondary indices
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  deletion_protection_enabled = true
-}
-
 resource "aws_dynamodb_table" "refresh_tokens" {
   name         = "RefreshTokens-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
