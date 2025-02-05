897594cb9f6b0e541b28feba9b0738dbdfd33892
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-05 11:02:50 +0100

    wip

--- src/lambdas/schedule/fetch-user-calendars/index.ts
+++ src/lambdas/schedule/fetch-user-calendars/index.ts
@@ -1,62 +1,37 @@
-import type { EventBridgeEvent } from 'aws-lambda';
-
-import { readEnv } from '@services/common/config';
-
-import { snsClient } from '@clients/sns';
-import { PublishCommand, type PublishCommandOutput } from '@aws-sdk/client-sns';
 import { UserLiveIndexStore } from '@services/stores/user-live-index-store';
+import { configMiddleware } from '@common/lambda-middleware';
+import { type FetchUserCalendarsConfig, readFetchUserCalendarsConfig } from './config';
+import type { z } from 'zod';
+import { eventBridgeEventSchema } from '@model/lambda-events/EventBridgeEvents';
+import { publishToSNSCalendarMessage, transformItem } from './send-message';
 
+const eventSchema = eventBridgeEventSchema<FetchUserCalendarsConfig>();
+export type Event = z.infer<typeof eventSchema>;
 
+async function lambdaHandler(event: Event): Promise<void> {
+  const { userLiveIndexStoreConfig } = event.lambdaConfig;
 
-const sns = snsClient();
-
-const transformItems = <T>(items: T): T => {
-  // This function does user x calendar list and transforms the items into SNS messages
-  return items;
-};
-
-const sendSNSCalendarMessage = (
-  topic: string,
-  messageBody: string
-): Promise<PublishCommandOutput> => {
-  const publishCommand = new PublishCommand({
-    TopicArn: topic,
-    Message: messageBody
-  });
-  return sns.send(publishCommand);
-};
-
-async function lambdaHandler(event: EventBridgeEvent<'Scheduled event', string>): Promise<void> {
-  console.log('Lambda event: ');
-  console.log(JSON.stringify(event, null, 2));
-  
-  const env = readEnv();
-  const TABLE_NAME = env.get('USERS_TABLE_NAME').required().asString();
-  const TABLE_INDEX_NAME = env.get('LOCAL_USERS_INDEX_NAME').required().asString();
-  const TOPIC_ARN = env.get('FETCH_CALENDARS_TOPIC_ARN').required().asString();
-  
-  console.log('Table Name', TABLE_NAME);
-  console.log('Index Name', TABLE_INDEX_NAME);
-  console.log('Topic Name', TOPIC_ARN);
-
-  const config = {
-    tableName: TABLE_NAME,
-    indexName: TABLE_INDEX_NAME,
-    pageSize: 50,
-  };
-  
-  const userLiveProvider = UserLiveIndexStore.withConfig(config)
-
+  const userLiveProvider = UserLiveIndexStore.withConfig(userLiveIndexStoreConfig);
   const liveUsersPaginator = userLiveProvider.getLiveUsers();
-  
+
   let totalItems = 0;
   for await (const liveUsersPage of liveUsersPaginator) {
     console.log('Processing new page of results');
+    console.log('First item of the page:', JSON.stringify(liveUsersPage[0], null, 2));
     totalItems += liveUsersPage.length;
+
+    const sendCalendarFoundMessagePromises = liveUsersPage
+      .map(transformItem)
+      .map(publishToSNSCalendarMessage);
+
+    await Promise.all(sendCalendarFoundMessagePromises);
   }
 
   // IF this lambda throws an error, would it go to a DLQ?
   console.log(`${totalItems} users processed`);
 }
 
-export const handler = lambdaHandler;
+// export const handler = lambdaHandler;
+export const handler = configMiddleware(() => readFetchUserCalendarsConfig()).handler<Event>(
+  lambdaHandler
+);
