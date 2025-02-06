bc265dbaafcc33c92e2bf30815aaf8df90129cde
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-02-06 13:51:42 +0100

    feat: contacts service and google impl

---
+++ src/services/google/people.test.ts
@@ -0,0 +1,73 @@
+/* eslint-disable camelcase */
+import type { Email } from '@notifycal/shared/types';
+import { google, type people_v1 } from 'googleapis';
+import type { GaxiosResponse } from 'googleapis-common';
+import { describe, expect, it, vi } from 'vitest';
+import { GooglePeople, type PhoneNumber } from './people';
+
+describe('GooglePeople Service', () => {
+  const validGooglePeopleResponse: GaxiosResponse<people_v1.Schema$SearchResponse> = {
+    data: {
+      results: [
+        {
+          person: {
+            phoneNumbers: [{ canonicalForm: '+1234567890' }, { value: '+0987654321' }]
+          }
+        }
+      ]
+    },
+    config: {},
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    request: {
+      responseURL: ''
+    }
+  };
+
+  it('should fetch contact by email and return phone numbers', () => {
+    const searchContactsFn = () => Promise.resolve(validGooglePeopleResponse);
+
+    return testit(searchContactsFn).then((result) => {
+      expect(result).toStrictEqual(['+1234567890', '+0987654321'] as Array<PhoneNumber>);
+    });
+  });
+
+  it('should return undefined if no phone numbers are found', () => {
+    const emptyResponse: GaxiosResponse<people_v1.Schema$SearchResponse> = {
+      data: { results: [] },
+      config: {},
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      request: { responseURL: '' }
+    };
+    const searchContactsFn = () => Promise.resolve(emptyResponse);
+
+    return testit(searchContactsFn).then((result) => {
+      expect(result).toBeUndefined();
+    });
+  });
+
+  it('should throw an error if the API call fails', () => {
+    const error = new Error('Boom! Google API Failure');
+    const searchContactsFn = () => Promise.reject(error);
+
+    const result = testit(searchContactsFn);
+
+    return expect(result).rejects.toThrow(error.message);
+  });
+
+  function testit(
+    searchContactsFn: () => Promise<GaxiosResponse<people_v1.Schema$SearchResponse>>
+  ): Promise<Array<PhoneNumber> | undefined> {
+    vi.mock('googleapis');
+    vi.mocked(google.people).mockReturnValue({
+      people: {
+        searchContacts: vi.fn().mockImplementation(searchContactsFn)
+      }
+    } as unknown as people_v1.People);
+    const config = { clientId: 'id', clientSecret: 'secret', redirectUri: 'uri' };
+    return GooglePeople.withRefreshToken(config, '').getPhoneNumbersBy('test@example.com' as Email);
+  }
+});
