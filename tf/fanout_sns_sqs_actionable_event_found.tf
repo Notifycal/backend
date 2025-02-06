65c80608e28d2e8bb7081cf5450c654d9f6fa024
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-06 20:29:21 +0100

    wire FindActionableEvents lambda with queue

--- tf/fanout_sns_sqs_actionable_event_found.tf
+++ tf/fanout_sns_sqs_actionable_event_found.tf
@@ -13,6 +13,6 @@ module "actionable_event_found_queue" {
   source       = "./modules/sqs"
   queue_name   = "user-calendar-fetched-${var.environment}"
   sender_arn   = module.actionable_event_found_topic.sns_topic_arn
-  receiver_arn = "" //TODO second lambda arn
+  receiver_arn = "" //TODO third lambda arn
   tags         = local.common_tags
 }
--- tf/fanout_sns_sqs_user_calendar_fetched.tf
+++ tf/fanout_sns_sqs_user_calendar_fetched.tf
@@ -13,6 +13,6 @@ module "user_calendar_fetched_queue" {
   source       = "./modules/sqs"
   queue_name   = "user-calendar-fetched-${var.environment}"
   sender_arn   = module.user_calendar_fetched_topic.sns_topic_arn
-  receiver_arn = "" //TODO second lambda arn
+  receiver_arn = module.find_actionable_events_lambda_alias.lambda_alias_arn
   tags         = local.common_tags
 }
