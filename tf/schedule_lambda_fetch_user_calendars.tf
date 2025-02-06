cab9008f5afd3a73b25344e2aaab54b11402047c
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-06 15:17:33 +0100

    use lambda alias ARN for event bridge trigger

--- tf/schedule_lambda_fetch_user_calendars.tf
+++ tf/schedule_lambda_fetch_user_calendars.tf
@@ -43,9 +43,7 @@ resource "aws_cloudwatch_event_rule" "fetch_user_calendars_trigger_rule" {
 resource "aws_cloudwatch_event_target" "fetch_user_calendars_event_target" {
   rule      = aws_cloudwatch_event_rule.fetch_user_calendars_trigger_rule.name
   target_id = "FetchLiveUsersCalendars"
-
-  # Does this support alias ARN or just the base function?
-  arn = module.fetch_user_calendars_lambda.lambda_function_arn
+  arn       = module.fetch_user_calendars_lambda_alias.lambda_alias_arn
 }
 
 module "fetch_user_calendars_lambda" {
