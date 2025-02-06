9bed309d415108a2d3d8ae2ea1be4813bf03b8c9
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-06 15:57:39 +0100

    add missing env var: USER_CALENDAR_FETCHED_TOPIC_ARN

--- tf/schedule_lambda_fetch_user_calendars.tf
+++ tf/schedule_lambda_fetch_user_calendars.tf
@@ -74,6 +74,7 @@ module "fetch_user_calendars_lambda" {
   environment_variables = merge({
     LIVE_USERS_INDEX_NAME     = local.live_users_index_name
     FETCH_CALENDARS_TOPIC_ARN = "TODO"
+    USER_CALENDAR_FETCHED_TOPIC_ARN = module.user_calendar_fetched_topic.sns_topic_arn
   }, local.protected_endpoint_env_vars, local.users_persistance_env_vars)
 }
 
