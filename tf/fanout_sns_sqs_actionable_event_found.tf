e0b14bbbcf86dac274d522aa841fe87a2854c6ff
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-06 20:26:19 +0100

    put topic and queue in same file

--- tf/fanout_sns_sqs_actionable_event_found.tf
+++ tf/fanout_sns_sqs_actionable_event_found.tf
@@ -8,3 +8,11 @@ module "actionable_event_found_topic" {
   }
   tags = local.common_tags
 }
+
+module "actionable_event_found_queue" {
+  source       = "./modules/sqs"
+  queue_name   = "user-calendar-fetched-${var.environment}"
+  sender_arn   = module.actionable_event_found_topic.sns_topic_arn
+  receiver_arn = "" //TODO second lambda arn
+  tags         = local.common_tags
+}
--- tf/sqs_user_calendar_fetched.tf
+++
@@ -1,7 +0,0 @@
-module "user_calendar_fetched_queue" {
-  source       = "./modules/sqs"
-  queue_name   = "user-calendar-fetched-${var.environment}"
-  sender_arn   = module.user_calendar_fetched_topic.sns_topic_arn
-  receiver_arn = "" //TODO second lambda arn
-  tags         = local.common_tags
-}
