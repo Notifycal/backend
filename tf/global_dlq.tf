0ff18996d057bac17f1d845aa0e2faae6ba345e5
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-11 19:47:09 +0100

    add comment

--- tf/global_dlq.tf
+++ tf/global_dlq.tf
@@ -10,6 +10,7 @@ resource "aws_sqs_queue_redrive_allow_policy" "dlq_redrive_allow_policy" {
   redrive_allow_policy = jsonencode({
     redrivePermission = "byQueue",
     sourceQueueArns = [
+      # Add SQS queue ARNs that have a DLQ associated here
       module.user_calendar_fetched_queue.sqs_queue_arn,
       module.actionable_event_found_queue.sqs_queue_arn
     ]
