35ddf46c5cf0f5794795ee3295de987779a262fe
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-13 15:44:30 +0100

    add tf for 3rd lambda

---
+++ tf/sqs_lambda_send_event_reminder.tf
@@ -0,0 +1,72 @@
+data "aws_iam_policy_document" "send_event_reminder_iam_policydoc" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:ReceiveMessage"
+    ]
+
+    resources = [
+      module.actionable_event_found_queue.sqs_queue_arn
+    ]
+  }
+}
+
+module "send_event_reminder_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 7.17"
+
+  function_name          = "send-event-reminder-${var.environment}"
+  publish                = local.lambdas_publish
+  create_package         = local.lambdas_create_package
+  local_existing_package = "${path.root}/../dist/lambdas/sqs/send-event-reminder.zip"
+
+  runtime     = var.lambdas_runtime
+  timeout     = local.api_lambdas_timeout
+  memory_size = 256
+  handler     = var.lambdas_handler_name
+
+  logging_log_format    = var.lambdas_logging_log_format
+  attach_tracing_policy = local.lambdas_attach_tracing_policy
+  tracing_mode          = local.lambdas_tracing_mode
+
+  # These 2 go together, if create_async_event_config is set to false (its default),
+  # lambdas will retry up to 2 times
+  create_async_event_config = true
+  maximum_retry_attempts    = 0
+  attach_dead_letter_policy = true
+  dead_letter_target_arn    = aws_sqs_queue.global_dlq_lambda.arn
+
+  tags = local.common_tags
+
+  attach_policy_json = true
+  policy_json        = data.aws_iam_policy_document.send_event_reminder_iam_policydoc.json
+
+  event_source_mapping = {
+    sqs = {
+      event_source_arn = module.actionable_event_found_queue.sqs_queue_arn
+      scaling_config = {
+        maximum_concurrency = 20
+      }
+      metrics_config = {
+        metrics = ["EventCount"]
+      }
+
+      # TODO: RESEARCH
+      # function_response_types = ["ReportBatchItemFailures"]
+    }
+  }
+
+  allowed_triggers = {
+    AllowSQSInvoke = {
+      principal  = "sqs.amazonaws.com"
+      source_arn = module.actionable_event_found_queue.sqs_queue_arn
+    }
+  }
+
+  environment_variables = merge({
+    # TODO: Vonage API Key
+  }, local.common_lambda_env_vars)
+}
