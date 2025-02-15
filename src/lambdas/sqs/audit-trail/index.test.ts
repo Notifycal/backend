9ed320cf34cb977c6e57ea5b4cbdfb42b68d99e4
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-02-15 18:16:46 +0100

    extract test suite and reuse it

--- src/lambdas/sqs/audit-trail/index.test.ts
+++ src/lambdas/sqs/audit-trail/index.test.ts
@@ -4,78 +4,39 @@ import {
   setEnvAuditTrailBaseStoreConfig,
   setEnvAuditTrailRecordExpiresAtConfig
 } from '@testing/utils/config';
-import type { Context, SQSEvent, SQSRecord } from 'aws-lambda';
-import { describe, expect, it, vi } from 'vitest';
-import { type Event, handler } from '.';
+import type { SQSEvent, SQSRecord } from 'aws-lambda';
+import { describe } from 'vitest';
+import { handler } from '.';
+import { createSqsHandlerTestSuite } from '../sqs-lambda-handler-test.suite';
 import type { AuditTrailConfig } from './config';
-import * as recordProcessor from './record-processor';
+import * as recordProcessorModule from './record-processor';
 
-const defaultConfig: AuditTrailConfig = {
-  auditTrailBaseStoreConfig: {
-    tableName: 'some-table-name'
-  },
-  recordExpiresAtConfig: {
-    expiresAtInDays: 7
-  }
-};
 const validUserCalendarFetchedEvent = userCalendarFetchedEvent;
 const validSqsRecord: SQSRecord = validRawRecord(validUserCalendarFetchedEvent);
 const validSqsBatchEvent: SQSEvent = {
   Records: [validSqsRecord]
 };
 
-describe('Audit trail', () => {
-  it('should parse config and events', () => {
-    vi.spyOn(recordProcessor, 'recordProcessor').mockResolvedValue();
-    return testit(validSqsBatchEvent).then((r) => {
-      expect(r).toStrictEqual({
-        batchItemFailures: []
-      });
-    });
-  });
-
-  it('should indicate partial failure in response', () => {
-    const processorSpy = vi
-      .spyOn(recordProcessor, 'recordProcessor')
-      .mockResolvedValueOnce()
-      .mockImplementation(() => {
-        throw new Error('Boom!');
-      });
-    const eventError: SQSRecord = { ...validSqsRecord, messageId: 'messageWithErrorId' };
-    const validInput: SQSEvent = {
-      Records: [validSqsRecord, eventError]
-    };
-    return testit(validInput).then((r) => {
-      expect(processorSpy).toHaveBeenCalledTimes(2);
-      expect(r).toStrictEqual({
-        batchItemFailures: [{ itemIdentifier: eventError.messageId }]
-      });
-    });
-  });
-
-  it('should throw an error if processing of every item fails', () => {
-    vi.spyOn(recordProcessor, 'recordProcessor').mockRejectedValue(new Error('Boom!'));
-    return expect(testit(validSqsBatchEvent)).rejects.toThrow(
-      'All records failed processing. See individual errors below.'
-    );
-  });
-
-  it('should fail to parse an event', () => {
-    const invalidEvent = { Records: [{ someField: 'someValue' }] };
-    return expect(
-      testit(invalidEvent as unknown as SQSEvent)
-    ).toRejectWithErrorContainingMessageParts([
-      'Lambda payload does not satisfy the schema. Error: Failed to parse schema. This error was caused by:'
-    ]);
-  });
-});
-
-function testit(event: SQSEvent, config: AuditTrailConfig = defaultConfig): Promise<void> {
-  setEnv(config);
-  return handler(event as unknown as Event, {} as Context);
-}
-
-function setEnv(config: AuditTrailConfig) {
+function setEnv() {
+  const config: AuditTrailConfig = {
+    auditTrailBaseStoreConfig: {
+      tableName: 'some-table-name'
+    },
+    recordExpiresAtConfig: {
+      expiresAtInDays: 7
+    }
+  };
   setEnvAuditTrailBaseStoreConfig(config.auditTrailBaseStoreConfig);
   setEnvAuditTrailRecordExpiresAtConfig(config.recordExpiresAtConfig);
 }
+
+describe(
+  // eslint-disable-next-line vitest/valid-describe-callback
+  'Audit Trail',
+  createSqsHandlerTestSuite({
+    handler,
+    setEnv,
+    validSqsBatchEvent,
+    recordProcessorModule
+  })
+);
--- src/lambdas/sqs/find-actionable-events/index.test.ts
+++ src/lambdas/sqs/find-actionable-events/index.test.ts
@@ -7,80 +7,41 @@ import {
   setEnvDeadLetterQueueConfig,
   setEnvIdpConfigs
 } from '@testing/utils/config';
-import type { Context, SQSEvent, SQSRecord } from 'aws-lambda';
-import { describe, expect, it, vi } from 'vitest';
-import { type Event, handler } from '.';
+import type { SQSEvent, SQSRecord } from 'aws-lambda';
+import { describe } from 'vitest';
+import { handler } from '.';
+import { createSqsHandlerTestSuite } from '../sqs-lambda-handler-test.suite';
 import type { ActionableEventsConfig } from './config';
-import * as recordProcessor from './record-processor';
+import * as recordProcessorModule from './record-processor';
 
-const defaultEnv: ActionableEventsConfig = {
-  actionableEventFoundTopicConfig: {
-    topicArn: 'someTopicArn' as AwsArn
-  },
-  deadLetterQueueConfig: {
-    queueUrl: 'http://aws.com/dql' as Url
-  },
-  idpConfigs: fakeIdpConfigs
-};
 const validUserCalendarFetchedEvent = userCalendarFetchedEvent;
 const validSqsRecord: SQSRecord = validRawRecord(validUserCalendarFetchedEvent);
 const validSqsBatchEvent: SQSEvent = {
   Records: [validSqsRecord]
 };
 
-describe('Find actionable events', () => {
-  it('should parse config and events', () => {
-    vi.spyOn(recordProcessor, 'recordProcessor').mockResolvedValue();
-    return testit(validSqsBatchEvent).then((r) => {
-      expect(r).toStrictEqual({
-        batchItemFailures: []
-      });
-    });
-  });
-
-  it('should indicate partial failure in response', () => {
-    const processorSpy = vi
-      .spyOn(recordProcessor, 'recordProcessor')
-      .mockResolvedValueOnce()
-      .mockImplementation(() => {
-        throw new Error('Boom!');
-      });
-    const eventError: SQSRecord = { ...validSqsRecord, messageId: 'messageWithErrorId' };
-    const validInput: SQSEvent = {
-      Records: [validSqsRecord, eventError]
-    };
-    return testit(validInput).then((r) => {
-      expect(processorSpy).toHaveBeenCalledTimes(2);
-      expect(r).toStrictEqual({
-        batchItemFailures: [{ itemIdentifier: eventError.messageId }]
-      });
-    });
-  });
-
-  it('should throw an error if processing of every item fails', () => {
-    vi.spyOn(recordProcessor, 'recordProcessor').mockRejectedValue(new Error('Boom!'));
-    return expect(testit(validSqsBatchEvent)).rejects.toThrow(
-      'All records failed processing. See individual errors below.'
-    );
-  });
-
-  it('should fail to parse an event', () => {
-    const invalidEvent = { Records: [{ someField: 'someValue' }] };
-    return expect(
-      testit(invalidEvent as unknown as SQSEvent)
-    ).toRejectWithErrorContainingMessageParts([
-      'Lambda payload does not satisfy the schema. Error: Failed to parse schema. This error was caused by:'
-    ]);
-  });
-});
-
-function testit(event: SQSEvent, config: ActionableEventsConfig = defaultEnv): Promise<void> {
-  setEnv(config);
-  return handler(event as unknown as Event, {} as Context);
-}
-
-function setEnv(config: ActionableEventsConfig) {
+function setEnv(): void {
+  const config: ActionableEventsConfig = {
+    actionableEventFoundTopicConfig: {
+      topicArn: 'someTopicArn' as AwsArn
+    },
+    deadLetterQueueConfig: {
+      queueUrl: 'http://aws.com/dql' as Url
+    },
+    idpConfigs: fakeIdpConfigs
+  };
   setEnvActionableEventFoundTopicConfig(config.actionableEventFoundTopicConfig);
   setEnvDeadLetterQueueConfig(config.deadLetterQueueConfig);
   setEnvIdpConfigs(config.idpConfigs);
 }
+
+describe(
+  // eslint-disable-next-line vitest/valid-describe-callback
+  'Find actionable events',
+  createSqsHandlerTestSuite({
+    handler,
+    setEnv,
+    validSqsBatchEvent,
+    recordProcessorModule
+  })
+);
