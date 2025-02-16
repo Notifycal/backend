4b9ecbb3e8f33894439fab853ac5a2637e83197a
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-16 14:33:05 +0100

    add naive MessagingService implementation

---
+++ src/services/messaging/index.ts
@@ -0,0 +1,36 @@
+import { Vonage } from '@vonage/server-sdk';
+import { Auth } from '@vonage/auth';
+import { RCSText } from '@vonage/messages';
+
+import { extractErrorMessage, throwError } from '@services/common/error-handling';
+
+import type { Brand, PhoneNumber, Uuid } from '@notifycal/shared/types';
+
+export type VonageApplicationId = Brand<string, 'ApplicationId'>;
+export type VonagePrivateKey = Brand<string, 'PrivateKey'>;
+
+export class MessagingService {
+  protected _client: Vonage;
+  
+  public constructor(applicationId: VonageApplicationId, privateKey: VonagePrivateKey) {
+    this._client = new Vonage(new Auth({
+      privateKey,
+      applicationId,
+    }));
+  }
+
+  public async sendMessage(messageBody: string, phoneNumber: PhoneNumber, clientRef: string): Promise<Uuid | void> {
+    try {
+      const { messageUUID } = await this._client.messages.send(new RCSText({
+        to: phoneNumber,
+        from: '',
+        clientRef,
+        text: messageBody
+      }));
+
+      return messageUUID as unknown as Uuid;
+    } catch (error) {
+      throwError(`Vonage API failed to send the reminder: ${clientRef}. Error: ${extractErrorMessage(error)}`);
+    }
+  }
+}
