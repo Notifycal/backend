db7438d05217a880a7617cb428c53b32765ff41b
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-02-17 14:44:41 +0100

    fix: previous commit

--- tf/sqs_lambda_audit_trail.tf
+++ tf/sqs_lambda_audit_trail.tf
@@ -58,7 +58,7 @@ module "audit_trail_lambda" {
   create_async_event_config = true
   maximum_retry_attempts    = 0
   attach_dead_letter_policy = true
-  dead_letter_target_arn    = aws_sqs_queue.global_dlq_unprocessable_lambda
+  dead_letter_target_arn    = aws_sqs_queue.global_dlq_unprocessable_lambda.arn 
 
   tags = local.common_tags
 
