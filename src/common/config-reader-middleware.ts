e551f303adf187e634f900b919fe323e8cc59419
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-26 22:28:49 +0100

    feat: async config-reader for SSM parameter retrieval

--- src/common/config-reader-middleware.ts
+++ src/common/config-reader-middleware.ts
@@ -9,27 +9,29 @@ import { logger } from './powertools';
 
 function configReader<TConfig, TResult>(
   request: Request<EventWithConfig<TConfig>, TResult, Error, Context>,
-  configReaderFn: () => TConfig,
+  configReaderFn: () => Promise<TConfig>,
   isApiRequest: boolean = true
-): TResult | void {
-  try {
-    const config = configReaderFn();
-    request.event.lambdaConfig = config;
-  } catch (error: unknown) {
-    if (isApiRequest) {
-      return errorHandler(500)(
-        `Endpoint config could not be loaded. Error: ${extractErrorMessage(error)}`
-      ) as TResult;
-    } else {
-      const errorMsg = `Lambda config could not be loaded. Error: ${extractErrorMessage(error)}`;
-      logger.error(errorMsg);
-      throw new Error(errorMsg);
+): Promise<TResult | void> {
+  return configReaderFn().then(
+    (config) => {
+      request.event.lambdaConfig = config;
+    },
+    (error) => {
+      if (isApiRequest) {
+        return errorHandler(500)(
+          `Endpoint config could not be loaded. Error: ${extractErrorMessage(error)}`
+        ) as TResult;
+      } else {
+        const errorMsg = `Lambda config could not be loaded. Error: ${extractErrorMessage(error)}`;
+        logger.error(errorMsg);
+        return Promise.reject(new Error(errorMsg));
+      }
     }
-  }
+  );
 }
 
 export function configReaderMiddleware<TConfig, TResult>(
-  configReaderFn: () => TConfig,
+  configReaderFn: () => Promise<TConfig>,
   isApiRequest: boolean
 ): MiddlewareObj<EventWithConfig<TConfig>, TResult> {
   const before: middy.MiddlewareFn<EventWithConfig<TConfig>, TResult> = (req) =>
