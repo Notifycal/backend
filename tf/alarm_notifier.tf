922bd68d241a5592a901ea2055d5802fecc2b33c
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-03-11 17:46:05 +0100

    feat: observability first pass. Slack notifier. Global lambda alarms based on default Lambda service metrics. WIP: untested

---
+++ tf/alarm_notifier.tf
@@ -0,0 +1,16 @@
+module "notify_slack" {
+  source  = "terraform-aws-modules/notify-slack/aws"
+  version = "~> 5.0"
+
+  sns_topic_name = "${var.environment}-alarms"
+
+  slack_webhook_url = var.observability.slack_webhook_url
+  slack_channel     = var.observability.slack_channel
+  slack_username    = "reporter"
+
+  lambda_function_name = "notify_slack_${var.environment}"
+  lambda_description   = "Lambda function which sends notifications to Slack"
+  log_events           = true
+
+  tags = merge({}, local.common_tags)
+}
