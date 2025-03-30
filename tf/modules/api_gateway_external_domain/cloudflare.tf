fe7e8470e95a99a62c56fbea81096b922113a558
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-03-30 20:07:10 +0200

    fix: cloudflare v5 changes

--- tf/modules/api_gateway_external_domain/cloudflare.tf
+++ tf/modules/api_gateway_external_domain/cloudflare.tf
@@ -1,13 +1,16 @@
 data "cloudflare_zone" "main" {
-  name = var.base_domain
+  filter = {
+    name = var.base_domain
+  }
 }
 
 # This is on a per-API basis, it will cover more stages if we add them in the future
-resource "cloudflare_record" "main" {
-  zone_id = data.cloudflare_zone.main.id
+resource "cloudflare_dns_record" "main" {
+  zone_id = data.cloudflare_zone.main.zone_id
 
   name    = var.domain_prefix
   content = aws_api_gateway_domain_name.custom_domain.regional_domain_name
   type    = "CNAME"
   proxied = false
+  ttl     = 3600
 }
