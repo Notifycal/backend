da175e0ae4a824b824501beaf80d414dd8f9fa44
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-04-04 00:06:08 +0200

    amend last commit

--- tf/sqs_queue_audit_trail.tf
+++ tf/sqs_queue_audit_trail.tf
@@ -3,7 +3,9 @@ module "audit_trail_queue" {
   queue_name = "audit-trail-${var.environment}"
   sender_arns = toset([
     module.user_calendar_fetched_topic.sns_topic_arn,
-    module.actionable_event_found_topic.sns_topic_arn
+    module.actionable_event_found_topic.sns_topic_arn,
+    module.messaging_topic.topic_arn,
+    module.api_rest_topic.topic_arn
   ])
   receiver_arn = module.audit_trail_lambda.lambda_function_arn
   tags         = local.common_tags
