8d4689e809c99e02d7d828692962aa8826c6ecfa
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-04-11 15:04:21 +0200

    feat: create ResponseTime metrics for 3rd parties (#500)

---
+++ src/utils/withIntegrationMetrics.ts
@@ -0,0 +1,43 @@
+import { MetricUnit } from '@aws-lambda-powertools/metrics';
+import { metrics } from '@common/powertools';
+import type { IdpName } from '@notifycal/shared/types';
+
+interface MetricDimensions {
+  [key: string]: string;
+}
+
+type IntegrationVendorName = 'Vonage' | IdpName;
+
+export async function withIntegrationMetrics<T>(
+  vendor: IntegrationVendorName,
+  operation: string,
+  fn: () => Promise<T>,
+  extraDimensions: MetricDimensions = {}
+): Promise<T> {
+  const INTEGRATION_LABEL_SUFFIX = 'IntegrationCall';
+  const metricDimensions = {
+    ...extraDimensions,
+    vendor,
+    operation
+  };
+
+  const start = Date.now();
+
+  try {
+    const result = await fn();
+    metrics.addMetric(`${INTEGRATION_LABEL_SUFFIX}Success`, MetricUnit.Count, 1, metricDimensions);
+    return result;
+  } catch (error) {
+    metrics.addMetric(`${INTEGRATION_LABEL_SUFFIX}Failure`, MetricUnit.Count, 1, metricDimensions);
+    throw error;
+  } finally {
+    const duration = Date.now() - start;
+
+    metrics.addMetric(
+      `${INTEGRATION_LABEL_SUFFIX}ResponseTime`,
+      MetricUnit.Milliseconds,
+      duration,
+      metricDimensions
+    );
+  }
+}
