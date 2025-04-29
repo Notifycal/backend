5d266f39a5ad4ea6cb8bf70c0fe1c9f819a1f841
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-04-29 13:37:42 +0200

    add unit test

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,6 +1,4 @@
-import type { Base64Event } from '@lambdas/sqs/send-email';
-
 export function toBase64(object: object): string {
   const jsonString = JSON.stringify(object);
-  return Buffer.from(jsonString).toString('base64') as Base64Event;
+  return btoa(jsonString);
 }
