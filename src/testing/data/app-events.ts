b1f2d8d5dbb1441cfaef26baa8fa76a457042316
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-05-09 18:18:48 +0200

    test events

--- src/testing/data/app-events.ts
+++ src/testing/data/app-events.ts
@@ -1,3 +1,4 @@
+import type { ActionableEventFoundEvent } from '@model/app-events/ActionableEventFoundEvent';
 import type { NoPhoneNumberForCalendarEventFoundEvent } from '@model/app-events/NoPhoneNumberForCalendarEventFoundEvent';
 import type { UserCalendarFetchedEvent } from '@model/app-events/UserCalendarFetchedEvent';
 import type {
@@ -24,10 +25,10 @@ export const userCalendarFetchedEvent: UserCalendarFetchedEvent = {
   eventId: 'c1625a78-7337-4fd8-a6c4-a0afb9c0ceb9' as EventId,
   correlationId: 'c1625a78-7337-4fd8-a6c4-a0afb9c0ceb9' as CorrelationId,
   eventType: 'UserCalendarFetched',
-  happenedAt: '2023-01-01T00:00:00Z' as DateTime,
-  userId: '96f3d941-1155-4d50-ac5a-19345fb7e9ef' as UserId,
+  happenedAt: '2025-05-09T13:00:00Z' as DateTime,
+  userId: 'b150d276-e327-51fb-b455-34a87c1c8ecc' as UserId,
   idp: 'google.com',
-  idpId: 'google-123' as IdpId,
+  idpId: '123456789' as IdpId,
   data: {
     run: {
       lowerBoundStartTime: '2023-01-01T00:00:00Z' as DateTime,
@@ -99,16 +100,55 @@ export const auditTrailActionableEventFoundEvent: AuditTrailStoreRecord = {
   Origin: 'somewhere' as AuditTrailStoreRecordOrigin
 };
 
-export const noPhoneNumberForCalendarEventFoundEvent: NoPhoneNumberForCalendarEventFoundEvent = {
-  eventId: 'some-event-id' as EventId,
-  eventType: 'NoPhoneNumberForCalendarEventFound',
-  happenedAt: '2024-01-01T15:00:00Z' as DateTime,
-  correlationId: 'test-correlation-id' as CorrelationId,
-  userId: 'test-user-id' as UserId,
-  idp: 'google.com',
-  idpId: 'test-idp-id' as IdpId,
+export const validActionableEventEvent: ActionableEventFoundEvent = {
   data: {
-    eventIdCause: 'some-cause-event-id' as EventId,
+    receiverDetails: {
+      type: 'phone',
+      phoneNumber: '+34123456789' as PhoneNumberE164,
+      countryCode: 'ES'
+    },
+    run: {
+      lowerBoundStartTime: '2023-01-01T00:00:00Z' as DateTime,
+      upperBoundStartTime: '2023-01-01T00:29:59Z' as DateTime,
+      slidingWindowInMinutes: 30
+    },
+    calendar: {
+      id: 'some calendar id' as CalendarId,
+      name: 'some calendar name' as CalendarName
+    },
+    calendarEvent: {
+      id: 'event-1',
+      attendees: [{ id: 'attendee@test.com' }],
+      isAllDayEvent: false,
+      startTime: '2024-01-02T15:05:00Z' as DateTime,
+      timeZone: 'Europe/Madrid' as TimeZone
+    },
+    senderDetails: {
+      type: 'phone',
+      phoneNumber: '+34666999888' as PhoneNumberE164,
+      countryCode: 'ES'
+    },
+    message: `This is some message`
+  },
+  correlationId: '0de651ef-535e-4d2e-b9ff-7bf43f5aaaaa' as CorrelationId,
+  eventId: '0de651ef-535e-4d2e-b9ff-7bf43f5a01ac' as EventId,
+  userId: 'b150d276-e327-51fb-b455-34a87c1c8ecc' as UserId,
+  idp: 'google.com',
+  idpId: '123456789' as IdpId,
+  eventType: 'ActionableEventFound',
+  happenedAt: '2025-05-09T13:00:00Z' as DateTime
+};
+
+export const noPhoneNumberForCalendarEventFoundEvent: NoPhoneNumberForCalendarEventFoundEvent = {
+  eventId: '0de651ef-535e-4d2e-b9ff-7bf43f5a0000' as EventId,
+  eventType: 'NoPhoneNumberForCalendarEventFound',
+  happenedAt: '2025-05-09T13:00:00Z' as DateTime,
+  correlationId: '0de651ef-535e-4d2e-b9ff-7bf43f5a0000' as CorrelationId,
+  userId: 'b150d276-e327-51fb-b455-34a87c1c8ecc' as UserId,
+  idp: 'google.com',
+  idpId: '123456789' as IdpId,
+  data: {
+    eventIdCause: '0de651ef-535e-4d2e-b9ff-7bf43f5a0000' as EventId,
     run: {
       lowerBoundStartTime: '2024-01-02T15:00:00Z' as DateTime,
       upperBoundStartTime: '2024-01-02T15:29:59Z' as DateTime,
@@ -120,7 +160,7 @@ export const noPhoneNumberForCalendarEventFoundEvent: NoPhoneNumberForCalendarEv
     },
     calendarEvent: {
       id: 'event-1',
-      attendees: [{ id: 'attendee@test.com' }],
+      attendees: [{ id: 'sergio.anger@gmail.com' }],
       isAllDayEvent: false,
       startTime: '2024-01-02T15:05:00Z' as DateTime,
       timeZone: 'Europe/Madrid' as TimeZone
