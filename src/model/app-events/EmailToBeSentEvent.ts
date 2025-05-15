bfab434b49993330abea3603b81aac101db86672
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-05-15 22:31:49 +0200

    chore: zod improvements. replace .brand by .transform to avoid arbitr… (#623)
    …ary strings all over the place

--- src/model/app-events/EmailToBeSentEvent.ts
+++ src/model/app-events/EmailToBeSentEvent.ts
@@ -1,12 +1,18 @@
 import { emailSchema } from '@notifycal/shared/schemas';
+import type {
+  ContentType,
+  EmailHtmlBody,
+  EmailInlineAttachementBase64,
+  EmailSubject
+} from '@own-types/model';
 import { z } from 'zod';
 import { eventSchemaGenerator, noPhoneNumberForCalendarEventFoundEventType } from './BaseEvent';
 import { emailWithNameSchema } from './common';
 
 const inlineAttachmentSchema = z.object({
   type: z.literal('inline'),
-  base64Content: z.string().brand('EmailInlineAttachementBase64'),
-  contentType: z.string().brand('ContentType')
+  base64Content: z.string().transform((data) => data as EmailInlineAttachementBase64),
+  contentType: z.string().transform((data) => data as ContentType)
 });
 export type EmailInlineAttachment = z.infer<typeof inlineAttachmentSchema>;
 
@@ -15,8 +21,8 @@ const inlineAttachments = z.record(inlineAttachmentSchema);
 const dataSchema = z.object({
   from: emailWithNameSchema,
   to: emailSchema,
-  subject: z.string().brand('EmailSubject'),
-  htmlBody: z.string().brand('EmailHtmlBody'),
+  subject: z.string().transform((data) => data as EmailSubject),
+  htmlBody: z.string().transform((data) => data as EmailHtmlBody),
   tags: z.string().array().default([]),
   subEventType: noPhoneNumberForCalendarEventFoundEventType,
   inlineAttachments: inlineAttachments,
