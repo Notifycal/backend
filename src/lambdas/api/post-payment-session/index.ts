990a559485ca8da7cd7159e3bbeb822244b3b0f2
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-06-03 13:11:08 +0200

    add metrics

--- src/lambdas/api/post-payment-session/index.ts
+++ src/lambdas/api/post-payment-session/index.ts
@@ -1,5 +1,7 @@
+import { MetricUnit } from '@aws-lambda-powertools/metrics';
 import { protectedEndpointMiddleware } from '@common/lambda-middleware';
-import { logger } from '@common/powertools';
+import { logger, metrics } from '@common/powertools';
+import type { MetricDimensions } from '@services/observability/metrics';
 import type { APIGatewayProxyResult, Context } from 'aws-lambda';
 import { StripeService } from '../../../services/stripe';
 import { readPostPaymentCheckoutSessionConfig } from './config';
@@ -21,6 +23,10 @@ async function lambdaHandler(
   const { tier, language } = event.body;
   const selectedTier = tiers[tier];
 
+  const dimensions: MetricDimensions = {
+    tier: selectedTier.id,
+    userId: userId
+  };
   return new StripeService(apiKey)
     .createCheckoutSession(
       userId,
@@ -33,17 +39,20 @@ async function lambdaHandler(
     .then(
       (sessionUrl) => {
         if (sessionUrl) {
+          metrics.addMetric('PaymentSessionCreated', MetricUnit.Count, 1, dimensions);
           return {
             statusCode: 200,
             body: JSON.stringify({ url: sessionUrl })
           };
         } else {
           logger.error(`No payment session was created for the user`);
+          metrics.addMetric('PaymentSessionCancelled', MetricUnit.Count, 1, dimensions);
           return failureResponse;
         }
       },
       (error) => {
         logger.error(`There was an error creating a payment session for the user`, { error });
+        metrics.addMetric('PaymentSessionFailed', MetricUnit.Count, 1, dimensions);
         return failureResponse;
       }
     );
