d26c641a3e2c43ec007da30bf360e67d9d67b005
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-06-03 13:16:19 +0200

    sort out commmented code

--- tf/modules/stripe/outputs.tf
+++ tf/modules/stripe/outputs.tf
@@ -10,40 +10,11 @@ output "subscription_tiers" {
   }
 }
 
-# # Tax configuration
-# output "spain_tax_rate" {
-#   description = "Spain VAT tax rate configuration"
-#   value = {
-#     id         = stripe_tax_rate.spain_vat.id
-#     percentage = stripe_tax_rate.spain_vat.percentage
-#     inclusive  = stripe_tax_rate.spain_vat.inclusive
-#   }
-# }
-
-# # Quick reference for frontend/backend integration
-# output "integration_config" {
-#   description = "Configuration summary for application integration"
-#   value = {
-#     currency    = var.currency
-#     environment = var.environment
-
-#     # Subscription upgrade paths for upsells
-#     upgrade_paths = {
-#       good_to_better = {
-#         from_price_id = stripe_price.monthly_prices["good"].id
-#         to_price_id   = stripe_price.monthly_prices["better"].id
-#         price_diff    = (var.subscription_tiers.better.price_cents - var.subscription_tiers.good.price_cents) / 100
-#       }
-#       good_to_best = {
-#         from_price_id = stripe_price.monthly_prices["good"].id
-#         to_price_id   = stripe_price.monthly_prices["best"].id
-#         price_diff    = (var.subscription_tiers.best.price_cents - var.subscription_tiers.good.price_cents) / 100
-#       }
-#       better_to_best = {
-#         from_price_id = stripe_price.monthly_prices["better"].id
-#         to_price_id   = stripe_price.monthly_prices["best"].id
-#         price_diff    = (var.subscription_tiers.best.price_cents - var.subscription_tiers.better.price_cents) / 100
-#       }
-#     }
-#   }
-# }
+output "spain_tax_rate" {
+  description = "Spain VAT tax rate configuration"
+  value = {
+    id         = stripe_tax_rate.spain_vat.id
+    percentage = stripe_tax_rate.spain_vat.percentage
+    inclusive  = stripe_tax_rate.spain_vat.inclusive
+  }
+}
