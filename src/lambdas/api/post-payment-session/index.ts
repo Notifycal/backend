4651b598c88e69e39fb2af25143bb03acb109cdd
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-06-14 00:09:02 +0200

    use identity in payment session too

--- src/lambdas/api/post-payment-session/index.ts
+++ src/lambdas/api/post-payment-session/index.ts
@@ -13,7 +13,8 @@ async function lambdaHandler(
   /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
   ctx: Context
 ): Promise<APIGatewayProxyResult> {
-  const { userId, email } = event.requestContext.authorizer.payload;
+  const { userId, idp, idpId, email } = event.requestContext.authorizer.payload;
+  const identity = { userId, idp, idpId, email };
   const apiKey = event.lambdaConfig.stripeAuthConfig.apiKey;
   const { successRedirectUrl, cancelRedirectUrl } = event.lambdaConfig.stripeCheckoutConfig;
   const { tiers } = event.lambdaConfig.paymentPlans;
@@ -25,14 +26,7 @@ async function lambdaHandler(
     userId: userId
   };
   return new StripeService(apiKey)
-    .createCheckoutSession(
-      userId,
-      email,
-      selectedTier,
-      language,
-      successRedirectUrl,
-      cancelRedirectUrl
-    )
+    .createCheckoutSession(identity, selectedTier, language, successRedirectUrl, cancelRedirectUrl)
     .then(
       (sessionUrl) => {
         if (sessionUrl) {
