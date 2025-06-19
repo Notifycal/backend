79d6734965b319bda7b65a7c07f2aa967914c9c8
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-06-19 23:01:02 +0200

    use hyphens for consistency with other parameters

--- tf/modules/stripe/ssm_payment_plans_info.tf
+++ tf/modules/stripe/ssm_payment_plans_info.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "payment_plans" {
-  name  = "/notifycal/${var.environment}/payment_plans"
+  name  = "/notifycal/${var.environment}/payment-plans"
   type  = "String"
   value = jsonencode(local.payment_plans)
 }
