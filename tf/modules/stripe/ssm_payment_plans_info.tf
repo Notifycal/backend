95f3ebea86d03b8b128ea60b0b3c2f6bb5657194
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-06-19 23:08:45 +0200

    amend parameter name

--- tf/modules/stripe/ssm_payment_plans_info.tf
+++ tf/modules/stripe/ssm_payment_plans_info.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "payment_plans" {
-  name  = "/notifycal/${var.environment}/payment-plans"
+  name  = "/notifycal/${var.environment}/tier-info"
   type  = "String"
   value = jsonencode(local.payment_plans)
 }
