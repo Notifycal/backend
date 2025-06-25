8970c21c89651bc034d490ab81096818a5d9576c
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-06-25 22:10:12 +0200

    fix: stripe events ingestion

--- src/utils/case.ts
+++
@@ -1,7 +0,0 @@
-export function toPascalCase(str: string): string {
-  return str
-    .replace(/_/g, ' ')
-    .split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join('');
-}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,45 +1,11 @@
-export type CapitalizeFirst<T extends string> = T extends `${infer First}${infer Rest}`
-  ? `${Uppercase<First>}${Rest}`
+export type ReplaceUnderscoreWithDot<T extends string> = T extends `${infer First}_${infer Rest}`
+  ? `${First}.${ReplaceUnderscoreWithDot<Rest>}`
   : T;
 
-type SplitByDot<T extends string> = T extends `${infer First}.${infer Rest}`
+export type SplitByDot<T extends string> = T extends `${infer First}.${infer Rest}`
   ? [First, ...SplitByDot<Rest>]
   : [T];
 
-type JoinPascalCase<T extends ReadonlyArray<string>> = T extends readonly [
-  infer First,
-  ...infer Rest
-]
-  ? First extends string
-    ? Rest extends ReadonlyArray<string>
-      ? Rest['length'] extends 0
-        ? CapitalizeFirst<First>
-        : `${CapitalizeFirst<First>}${JoinPascalCase<Rest>}`
-      : never
-    : never
-  : '';
-
-type InsertEventBeforeLast<T extends ReadonlyArray<string>> = T extends readonly [
-  ...infer Init,
-  infer Last
-]
-  ? Init extends ReadonlyArray<string>
-    ? Last extends string
-      ? [...Init, 'Event', Last]
-      : never
-    : never
-  : T extends readonly [infer Single]
-    ? Single extends string
-      ? [Single]
-      : never
-    : [];
-
-export type PascalCaseEventType<T extends string> = T extends string
-  ? SplitByDot<T> extends infer Parts
-    ? Parts extends ReadonlyArray<string>
-      ? Parts['length'] extends 1
-        ? T
-        : JoinPascalCase<InsertEventBeforeLast<Parts>>
-      : never
-    : never
-  : never;
+export type CapitalizeFirst<T extends string> = T extends `${infer First}${infer Rest}`
+  ? `${Uppercase<First>}${Rest}`
+  : T;
