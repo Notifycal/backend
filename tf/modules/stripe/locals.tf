33ae0d0b72acc1905d2c6c22614f2c34af7caef2
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-07-01 21:54:57 +0200

    improve some tf code

--- tf/modules/stripe/locals.tf
+++ tf/modules/stripe/locals.tf
@@ -1,8 +1,8 @@
 locals {
   create_payment_plan = {
     for key, config in merge(var.subscription_tiers, var.topups) : key => {
-      product_id          = key == keys(var.subscription_tiers)[0] || key == keys(var.subscription_tiers)[1] || key == keys(var.subscription_tiers)[2] ? stripe_product.subscription_tiers[key].id : stripe_product.topups[key].id
-      price_id            = key == keys(var.subscription_tiers)[0] || key == keys(var.subscription_tiers)[1] || key == keys(var.subscription_tiers)[2] ? stripe_price.monthly_prices[key].id : stripe_price.topup_prices[key].id
+      product_id          = try(stripe_product.subscription_tiers[key].id, stripe_product.topups[key].id)
+      price_id            = try(stripe_price.monthly_prices[key].id, stripe_price.topup_prices[key].id)
       name                = config.name
       price_eur           = config.price_cents / 100
       number_of_reminders = floor(config.price_cents / 100 / var.country_to_sms_cost_map["ES"])
