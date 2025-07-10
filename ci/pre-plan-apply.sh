577c2eb3c3d00a8ab8810106fd53cb5a5fb74b79
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-07-10 15:28:49 +0200

    fix(ci): use `latest` instead of `main`

--- ci/pre-plan-apply.sh
+++ ci/pre-plan-apply.sh
@@ -30,8 +30,8 @@ echo
 echo "Retrieving release from Github..."
 TMP_DIR=$(mktemp -d "/tmp/${STACK_NAME}.XXXXX")
 
-if [[ "${STACK_VERSION}" == "main" ]]; then
-  latest_release=$(gh release list --repo "${_GH_ORG}/${STACK_NAME}" --json name,isLatest |jq -rc '.[] | select(.isLatest)|.name')
+if [[ "${STACK_VERSION}" == "latest" ]]; then
+  latest_release=$(gh release list --repo "${_GH_ORG}/${STACK_NAME}" --json name,isLatest --jq '.[] | select(.isLatest)|.name')
   echo "Downloading the latest release (${latest_release}) as STACK_VERSION is 'main'"
   gh release download "${latest_release}" --repo "${_GH_ORG}/${STACK_NAME}" --dir "${TMP_DIR}"
 else
