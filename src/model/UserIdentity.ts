bd947c643ee5dcddfe05c2d4143db705df327a13
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-07-11 14:39:29 +0200

    extract some code and bump shared to import  it

--- src/model/UserIdentity.ts
+++ src/model/UserIdentity.ts
@@ -1,8 +1,4 @@
-import type { Identity, IdpName } from '@notifycal/shared/types';
-
-export type CapitalizeKeys<T> = {
-  [K in keyof T as Capitalize<K & string>]: T[K];
-};
+import type { CapitalizeKeys, Identity, IdpName } from '@notifycal/shared/types';
 
 export type UserIdentity<TIdpName> = CapitalizeKeys<Identity<TIdpName>>;
 
--- src/model/store/AuditTrailStoreRecord.ts
+++ src/model/store/AuditTrailStoreRecord.ts
@@ -7,9 +7,8 @@ import {
   systemSchema
 } from '@model/app-events/BaseEvent';
 import { eventIdSchema } from '@model/app-events/common';
-import type { CapitalizeKeys } from '@model/UserIdentity';
 import { dateTimeSchema, idpIdSchema, userIdSchema } from '@notifycal/shared/schemas';
-import type { Brand, Identity, IdpName } from '@notifycal/shared/types';
+import type { Brand, CapitalizeKeys, Identity, IdpName } from '@notifycal/shared/types';
 import { z } from 'zod';
 
 export const dataSchema = z.object({}).passthrough();
