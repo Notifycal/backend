5bc1ab4b659871ac041a8beaecf2ca057045288b
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-07-21 15:32:51 +0200

    fix: stripe webhook IaC

--- tf/modules/stripe-webhook/eventbridge.tf
+++ tf/modules/stripe-webhook/eventbridge.tf
@@ -20,7 +20,7 @@ resource "aws_cloudwatch_event_rule" "stripe_event_rules" {
   for_each       = try(var.integration_config.eventbridge.streams_to_return, toset([]))
   name           = "stripe-events-${each.value}-${var.environment}"
   description    = "Contains all Stripe events defined in Stripe event-destination API"
-  event_bus_name = aws_cloudwatch_event_bus.stripe["stripe"].name
+  event_bus_name = aws_cloudwatch_event_bus.stripe[0].name
   event_pattern = jsonencode({
     "source" = [{
       "prefix" : "aws.partner/stripe.com"
