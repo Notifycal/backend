4f687bcf4db03f2ac86a85d6dccc7b9cccc16e03
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-02 12:32:46 +0200

    extract zod scherma util

--- src/model/Sms.ts
+++ src/model/Sms.ts
@@ -1,9 +1,10 @@
+import { coerzableNumberSchema } from '@utils/zod';
 import type { count } from 'sms-length';
 import z from 'zod';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types
 export const smsLengthCountEstimateResultSchema = (coerced: boolean = false) => {
-  const coerceableNumberSchema = coerced ? z.coerce.number() : z.number();
+  const coerceableNumberSchema = coerzableNumberSchema(coerced);
   return z.object({
     encoding: z.union([z.literal('GSM_7BIT'), z.literal('GSM_7BIT_EXT'), z.literal('UTF16')]),
     length: coerceableNumberSchema,
