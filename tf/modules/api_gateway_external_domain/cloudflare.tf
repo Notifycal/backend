d115a402f28779509d4bea9d5df0e263bed377e0
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-06 02:08:34 +0200

    upgrade provider

--- tf/modules/api_gateway_external_domain/cloudflare.tf
+++ tf/modules/api_gateway_external_domain/cloudflare.tf
@@ -1,12 +1,14 @@
 data "cloudflare_zone" "main" {
-  name = var.base_domain
+  filter = {
+    name = var.base_domain
+  }
 }
 
 # This is on a per-API basis, it will cover more stages if we add them in the future
-resource "cloudflare_record" "main" {
-  zone_id = data.cloudflare_zone.main.id
+resource "cloudflare_dns_record" "main" {
+  zone_id = data.cloudflare_zone.main.zone_id
 
-  name    = var.domain_prefix
+  name    = "${var.domain_prefix}.${var.base_domain}"
   content = aws_api_gateway_domain_name.custom_domain.regional_domain_name
   type    = "CNAME"
   proxied = false
