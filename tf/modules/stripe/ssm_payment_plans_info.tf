6260d4b3e5399d832f8667fdcb4e74c2ff62cf37
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-08-28 13:31:15 +0200

    feat: country to sms cost map ssm param for service discovery (#1012)
    Co-authored-by: github-actions <github-actions@github.com>

--- tf/modules/stripe/ssm_payment_plans_info.tf
+++ tf/modules/stripe/ssm_payment_plans_info.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "payment_plans" {
   type  = "String"
   value = jsonencode(local.payment_plans)
 }
+
+resource "aws_ssm_parameter" "country_to_sms_cost_map" {
+  name  = "/notifycal/${var.environment}/country-to-sms-cost-map"
+  type  = "String"
+  value = jsonencode(var.country_to_sms_cost_map)
+}
