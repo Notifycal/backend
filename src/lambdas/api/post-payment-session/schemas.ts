17fbdbd178f5bbd1710036d3866a2643a56e77b9
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-11-27 15:14:00 +0100

    feat: free trial

--- src/lambdas/api/post-payment-session/schemas.ts
+++ src/lambdas/api/post-payment-session/schemas.ts
@@ -1,5 +1,6 @@
 import { JSONStringified } from '@aws-lambda-powertools/parser/helpers';
 import { authedEventSchema } from '@model/lambda-events/ApiGatewayEvents';
+import { freeTrialTierId } from '@model/PaymentPlans';
 import { languageCodeSchema, tierIds, topupIds } from '@notifycal/shared/schemas';
 import { z } from 'zod';
 import type { PostPaymentCheckoutSessionConfig } from './config';
@@ -10,6 +11,7 @@ const languagePartialSchema = {
 
 const _tierIdSchemas = tierIds.map((tier) => z.literal(tier));
 export const tierIdSchemas = z.union([
+  z.literal(freeTrialTierId),
   _tierIdSchemas[0]!,
   _tierIdSchemas[1]!,
   ..._tierIdSchemas.slice(2)
